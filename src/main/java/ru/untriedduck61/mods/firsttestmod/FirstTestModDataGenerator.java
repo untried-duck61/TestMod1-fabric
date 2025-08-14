55692eb830607a8831ce687a1bc0c8fb3fb2becc
Ivan-Al_20 <ialeshechkin0@gmail.com>
2025-08-14 18:54:49 +0400

    Initial commit

---
+++ src/main/java/ru/untriedduck61/mods/firsttestmod/FirstTestModDataGenerator.java
@@ -0,0 +1,11 @@
+package ru.untriedduck61.mods.firsttestmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FirstTestModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
