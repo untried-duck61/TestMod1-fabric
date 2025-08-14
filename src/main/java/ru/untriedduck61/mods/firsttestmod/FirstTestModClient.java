110109b02a63112010edbf810692b13aa5bd7024
Ivan-Al_20 <ialeshechkin0@gmail.com>
2025-08-14 19:12:37 +0400

    update code

--- src/main/java/ru/untriedduck61/mods/firsttestmod/FirstTestModClient.java
+++ src/main/java/ru/untriedduck61/mods/firsttestmod/FirstTestModClient.java
@@ -1,4 +1,11 @@
 package ru.untriedduck61.mods.firsttestmod;
 
-public class FirstTestModClient {
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstTestModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
